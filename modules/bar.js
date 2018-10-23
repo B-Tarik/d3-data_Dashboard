358b50b87b13dc746c054c9a102892d59c3b75ca
B-TARIK-P <dragonpiece007@gmail.com>
2018-10-23 22:35:39 +0000

    first commit

---
+++ modules/bar.js
@@ -0,0 +1,122 @@
+function createBar(width, height) {
+  const bar = d3.select('#bar')
+								.attr('width', width)
+								.attr('height', height);
+
+	bar.append('g')
+			.classed('x-axis', true);
+
+	bar.append('g')
+			.classed('y-axis', true);
+
+	bar.append('text')
+			.attr('transform', 'rotate(-90)')
+			.attr('x', - height / 2)
+			.attr('dy', '1em')
+			.style('text-anchor', 'middle')
+			.style('font-size', '1rem')
+			.classed('y-axis-label', true);
+
+	bar.append('text')
+			.attr('x', width / 2)
+			.attr('y', '1em')
+			.attr('font-size', '1.5rem')
+			.style('text-anchor', 'middle')
+			.classed('bar-title', true);
+}
+
+function highlightBars(year) {
+	d3.select('#bar')
+		.selectAll('rect')
+			.attr('fill', d => d.year === year ? '#16a085' : '#1abc9c');
+}
+
+function drawBar(data, dataType, country) {
+	const bar					= d3.select('#bar'),
+				padding 		= {
+												top: 30,
+												right: 30,
+												bottom: 30,
+												left: 110
+											},
+				barPadding	= 1,
+				width				= +bar.attr('width'),
+				height			= +bar.attr('height'),
+				countryData	= data.filter(d => d.country === country)
+													.sort((a, b) => a.year - b.year),
+
+				xScale			= d3.scaleLinear()
+												.domain(d3.extent(data, d => d.year))
+												.range([padding.left, width - padding.right]),
+
+				yScale			= d3.scaleLinear()
+												.domain([0, d3.max(countryData, d => d[dataType])])
+												.range([height - padding.bottom, padding.top]),
+
+				barWidth		= xScale(xScale.domain()[0] + 1) - xScale.range()[0],
+
+				xAxis				= d3.axisBottom(xScale)
+												.tickFormat(d3.format('.0f')),
+
+				yAxis				= d3.axisLeft(yScale),
+
+				axisLabel		= dataType === 'emissions' ?
+											'CO2 emissions, thousand metric tons' :
+											'CO2 emissions, metric tons per capita',
+
+				barTitle		= country ?
+											'CO2 Emissions, ' + country :
+											'Click on a country to see annual trenmds.',
+
+				t						= d3.transition()
+												.duration(1000)
+												.ease(d3.easeBounceOut),
+
+				update			= bar
+												.selectAll('.bar')
+												.data(countryData);
+
+	d3.select('.x-axis')
+		.attr('transform',`translate(0, ${height - padding.bottom})`)
+		.call(xAxis);			
+
+	d3.select('.y-axis')
+			.attr('transform',`translate(${padding.left - barWidth / 2}, 0)`)
+			.transition()
+			.duration(1000)
+			.call(yAxis);
+
+
+	d3.select('.y-axis-label')
+			.text(axisLabel);
+		
+	d3.select('.bar-title')
+			.text(barTitle);
+
+		
+	update
+		.exit()
+		.transition(t)
+			.delay((d, i, nodes) => (nodes.length - i - 1) * 100)
+			.attr('y', height - padding.bottom)
+			.attr('height', 0)
+			.remove();
+
+	update
+		.enter()
+		.append('rect')
+			.classed('bar', true)
+			.attr('y', height - padding.bottom)
+			.attr('height', 0)
+		.merge(update)
+			.attr('x', d => (xScale(d.year) + xScale(d.year - 1)) / 2)
+			.attr('width', barWidth - barPadding)
+			.transition(t)
+			.delay((d, i) =>  i * 100)
+				.attr('y', d => yScale(d[dataType]))
+				.attr('height', d => height - padding.bottom - yScale(d[dataType]));
+
+
+}
+
+export {createBar, drawBar, highlightBars}
---
+++ modules/tooltip.js
@@ -0,0 +1,49 @@
+function updateTooltip() {
+    const tooltip 	= d3.select('.tooltip'),
+					tgt				= d3.select(d3.event.target),
+					isCountry	= tgt.classed('country'),
+					isBar			= tgt.classed('bar'),
+					isArc			= tgt.classed('arc'),
+					dataType	= d3.select('input:checked')
+												.property('value'),
+					units			= dataType === 'emissions' ? 'thousand metric tons' : 'metric tons per capita';
+		let	data				= '',
+				percentage	= '';
+			
+		if(isCountry) data = tgt.data()[0].properties;
+		if(isArc) {
+			data 				= tgt.data()[0].data;
+			percentage	= `<p>Percentage of total: ${getPercentage(tgt.data()[0])}</p>`
+		}
+		if(isBar) data = tgt.data()[0];
+
+		tooltip
+			.style('opacity', +(isCountry || isArc || isBar))
+			.style('left', (d3.event.pageX - tooltip.node().offsetWidth / 2) + 'px')
+			.style('top', (d3.event.pageY - tooltip.node().offsetHeight - 10) + 'px');
+		
+		if (data) {
+			let dataValue = data[dataType] ?
+											data[dataType].toLocaleString() + ' ' + units :
+											'Data Not Available';
+			tooltip
+				.html(`
+					<p>Country: ${data.country}</p>
+					<p>${formatDataType(dataType)}: ${dataValue}</p>
+					<p>Year: ${data.year || d3.select('#year').property('value')}</p>
+					${percentage}
+				`)
+		}
+}
+
+function formatDataType(key) {
+	return key[0].toUpperCase() + key.slice(1).replace(/[A-Z]/g, c => ' ' + c);
+}
+
+function getPercentage(d) {
+	let angle 		= d.endAngle - d.startAngle,
+			fraction	= 100 * angle / (Math.PI * 2);
+	return fraction.toFixed(2) + '%';
+}
+
+export {updateTooltip};
